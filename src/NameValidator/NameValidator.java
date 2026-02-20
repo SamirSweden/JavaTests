93dccac63a1407749ba6f209551b008792aa38fd
Samir Azimov <samirazimov488@gmail.com>
2026-02-20 14:07:35 +0500

    Update NameValidator.java

--- src/NameValidator/NameValidator.java
+++ src/NameValidator/NameValidator.java
@@ -1,15 +1,13 @@
 public class NameValidator {
 
-    public boolean isValid(String name){
-        if (name == null) return  false;
-
-        if (name.length() < 3 || name.length() > 12) return  false;
-
-        for (char c: name.toCharArray()){
-            boolean ok = Character.isLetterOrDigit(c) || c != '_';
-            return  false;
+    public boolean isValid(String name) {
+        if (name == null) return false;
+        if (name.length() < 3 || name.length() > 12) return false;
+        for (char c : name.toCharArray()) {
+            boolean ok = Character.isLetterOrDigit(c) || c == '_';
+            if (!ok) return false;
         }
-
-        return  true;
+        return true;
     }
+    
 }
